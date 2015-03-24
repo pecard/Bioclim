adea86ebf5584b9f9498d49fe18b703d50a316cb
Paulo E. Cardoso <pauloeducardoso@gmail.com>
2015-03-24 10:11:56 +0000

    Update bioclim_model.R

--- R/bioclim_model.R
+++ R/bioclim_model.R
@@ -1,4 +1,4 @@
-#' Envelope Ambiental Bioclim
+#' Envelope Ambiental - Bioclim disponivel no package dismo
 #' Distribuicao com base em dados de presenca
 #' referencias:
 #' https://sites.google.com/site/rodriguezsanchezf/news/usingrasagis
@@ -17,40 +17,40 @@ new.packs <- kpacks[!(kpacks %in% installed.packages()[,"Package"])]
 if(length(new.packs)) install.packages(new.packs)
 lapply(kpacks, require, character.only=T)
 
-#' Administrative Data GADM
+#' Download of Administrative Data from GADM database
 mz_adm <-getData('GADM', country='MOZ', level= 1)
 mz_adm <- fortify(mz_adm)
 
-#' Protected Area Network
-panet <- rgdal::readOGR('..\\mz_prpotectedplanet.shp')
+#' Protected Area Network as obtained from Protected Planet
+panet <- rgdal::readOGR('~\\mz_prpotectedplanet.shp')
 proj4string(panet) = CRS('+init=epsg:4326') # wgs84
 pandf <- fortify(panet) #! spdf to dataframe
 
 
-#' Get Altitude data ----------------------------------------------------------
+#' Get Altitude data: SRTM -----------------------------------------------------
 alt <- getData('alt', country = 'MOZ', mask = F)
 plot(alt)
 
 #' Climate data ----------------------------------------------------------------
-#BIO1 = Annual Mean Temperature
-#BIO2 = Mean Diurnal Range (Mean of monthly (max temp - min temp))
-#BIO3 = Isothermality (BIO2/BIO7) (* 100)
-#BIO4 = Temperature Seasonality (standard deviation *100)
-#BIO5 = Max Temperature of Warmest Month
-#BIO6 = Min Temperature of Coldest Month
-#BIO7 = Temperature Annual Range (BIO5-BIO6)
-#BIO8 = Mean Temperature of Wettest Quarter
-#BIO9 = Mean Temperature of Driest Quarter
-#BIO10 = Mean Temperature of Warmest Quarter
-#BIO11 = Mean Temperature of Coldest Quarter
-#BIO12 = Annual Precipitation
-#BIO13 = Precipitation of Wettest Month
-#BIO14 = Precipitation of Driest Month
-#BIO15 = Precipitation Seasonality (Coefficient of Variation)
-#BIO16 = Precipitation of Wettest Quarter
-#BIO17 = Precipitation of Driest Quarter
-#BIO18 = Precipitation of Warmest Quarter
-#BIO19 = Precipitation of Coldest Quarter
+#'BIO1 = Annual Mean Temperature
+#'BIO2 = Mean Diurnal Range (Mean of monthly (max temp - min temp))
+#'BIO3 = Isothermality (BIO2/BIO7) (* 100)
+#'BIO4 = Temperature Seasonality (standard deviation *100)
+#'BIO5 = Max Temperature of Warmest Month
+#'BIO6 = Min Temperature of Coldest Month
+#'BIO7 = Temperature Annual Range (BIO5-BIO6)
+#'BIO8 = Mean Temperature of Wettest Quarter
+#'BIO9 = Mean Temperature of Driest Quarter
+#'BIO10 = Mean Temperature of Warmest Quarter
+#'BIO11 = Mean Temperature of Coldest Quarter
+#'BIO12 = Annual Precipitation
+#'BIO13 = Precipitation of Wettest Month
+#'BIO14 = Precipitation of Driest Month
+#'BIO15 = Precipitation Seasonality (Coefficient of Variation)
+#'BIO16 = Precipitation of Wettest Quarter
+#'BIO17 = Precipitation of Driest Quarter
+#'BIO18 = Precipitation of Warmest Quarter
+#'BIO19 = Precipitation of Coldest Quarter
 
 wcl <- getData('worldclim', var = 'bio', res = 2.5) # WorldClim Vars
 
